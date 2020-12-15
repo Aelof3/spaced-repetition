f0639d54e2524ca999dc5698b8d0bd6ad3485d9c
Aelof3 <folea3@gmail.com>
2020-12-15 14:26:17 -0500

    upload to repo

---
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from '../../contexts/UserContext';
+import UserService from '../../services/user-service';
+import WordList from '../../components/WordList/WordList';
+
+class DashboardRoute extends Component {
+  static contextType = UserContext;
+  state = {
+    width: `${this.context.score}%`,
+  };
+  //gets my language and words progress from the server
+  //shown my language
+  //shown the words to learn for the language
+  //shown my count for correct and incorrect responses for each word
+  //given a button/link to start learning
+  //shown the total score for guessing words correctly
+
+  //GET Language = API call and then stores language and words into context
+
+  componentDidMount() {
+    UserService.getUserData().then((data) => {
+      this.context.setLanguage(data.language.name);
+      this.context.setScore(data.language.total_score);
+      this.context.setWords(data.words);
+    });
+  }
+
+  render() {
+    return (
+      <section className="dashboard-main">
+        <div className="dashboard-header">
+          <h2>{this.context.language}</h2>
+          <p>Total correct answers: {this.context.score}</p>
+          <Link className="btn" to="/learn">
+            Start practicing
+          </Link>
+          <h3>Words to practice</h3>
+        </div>
+        <WordList />
+      </section>
+    );
+  }
+}
+
+export default DashboardRoute;
